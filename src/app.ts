5d41c54c39c91fad3f972c57893151bb25d9b697
hideo651 <hideo651@msn.com>
2023-03-23 21:17:32 -0300

    feat: adicionado o cors

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,10 @@ import express from "express";
 import { routes } from "./routes";
 import { errorMiddleware } from "./middlewares/error.middleware";
 
+import cors from "cors";
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errorMiddleware);
